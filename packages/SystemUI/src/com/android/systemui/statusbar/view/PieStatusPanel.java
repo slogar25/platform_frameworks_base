9e59eac6af19fc90f1117445396465ecde8e7c8c
slogar25 <michaelslogar@gmail.com>
2013-12-22 19:36:47 -0500

    PIE: clear all button
    Change-Id: I6333521999d846a417a0f8a3bba62a72aa876dbd
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/view/PieStatusPanel.java

--- packages/SystemUI/src/com/android/systemui/statusbar/view/PieStatusPanel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/view/PieStatusPanel.java
@@ -16,6 +16,10 @@
 
 package com.android.systemui.statusbar.view;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.database.ContentObserver;
@@ -46,6 +50,7 @@ import android.widget.ScrollView;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.NotificationData;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.PieControlPanel;
 
@@ -65,11 +70,19 @@ public class PieStatusPanel {
 
     private ScrollView mScrollView;
 
+
+
+    private View mClearButton;
+
     private View mContentFrame;
     private QuickSettingsContainerView mQS;
     private NotificationRowLayout mNotificationPanel;
     private PieControlPanel mPanel;
     private ViewGroup[] mPanelParents = new ViewGroup[2];
+
+    private Handler mHandler = new Handler();
+    private NotificationData mNotificationData;
+    private Runnable mPostCollapseCleanup = null;
 
     private int mCurrentViewState = -1;
     private int mFlipViewState = -1;
@@ -116,6 +129,11 @@ public class PieStatusPanel {
 
         mScrollView.setOnTouchListener(new ViewOnTouchListener());
         mContentFrame.setOnTouchListener(new ViewOnTouchListener());
+
+        mNotificationData = mPanel.getBar().getNotificationData();
+        mClearButton = (ImageView) mPanel.getBar().mContainer.findViewById(R.id.clear_all_button);
+        mClearButton.setOnClickListener(mClearButtonListener);
+
 
         mPanel.getBar().mContainer.setVisibility(View.GONE);
     }
@@ -165,6 +183,79 @@ public class PieStatusPanel {
 
     }
 
+    private View.OnClickListener mClearButtonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            synchronized (mNotificationData) {
+                // animate-swipe all dismissable notifications, then animate the shade closed
+                int numChildren = mNotificationPanel.getChildCount();
+
+                int scrollTop = mScrollView.getScrollY();
+                int scrollBottom = scrollTop + mScrollView.getHeight();
+                final ArrayList<View> snapshot = new ArrayList<View>(numChildren);
+                for (int i=0; i<numChildren; i++) {
+                    final View child = mNotificationPanel.getChildAt(i);
+                    if (mNotificationPanel.canChildBeDismissed(child) && child.getBottom() > scrollTop &&
+                            child.getTop() < scrollBottom) {
+                        snapshot.add(child);
+                    }
+                }
+                if (snapshot.isEmpty()) {
+                    return;
+                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Decrease the delay for every row we animate to give the sense of
+                        // accelerating the swipes
+                        final int ROW_DELAY_DECREMENT = 10;
+                        int currentDelay = 140;
+                        int totalDelay = 0;
+
+                        // Set the shade-animating state to avoid doing other work during
+                        // all of these animations. In particular, avoid layout and
+                        // redrawing when collapsing the shade.
+                        mNotificationPanel.setViewRemoval(false);
+
+                        mPostCollapseCleanup = new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    mNotificationPanel.setViewRemoval(true);
+                                    clearAll();
+                                } catch (Exception ex) { }
+                            }
+                        };
+
+                        View sampleView = snapshot.get(0);
+                        int width = sampleView.getWidth();
+                        final int velocity = width * 8; // 1000/8 = 125 ms duration
+                        for (final View _v : snapshot) {
+                            mHandler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mNotificationPanel.dismissRowAnimated(_v, velocity);
+                                }
+                            }, totalDelay);
+                            currentDelay = Math.max(50, currentDelay - ROW_DELAY_DECREMENT);
+                            totalDelay += currentDelay;
+                        }
+                        // Delay the collapse animation until after all swipe animations have
+                        // finished. Provide some buffer because there may be some extra delay
+                        // before actually starting each swipe animation. Ideally, we'd
+                        // synchronize the end of those animations with the start of the collaps
+                        // exactly.
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                hideNotificationsPanel();
+                            }
+                        }, totalDelay + 225);
+                    }
+                }).start();
+            }
+        }
+    };
+
     public int getFlipViewState() {
         return mFlipViewState;
     }
@@ -211,10 +302,12 @@ public class PieStatusPanel {
 
     public void showTilesPanel() {
         showPanel(mQS);
+        ShowClearAll(true);
     }
 
     public void showNotificationsPanel() {
         showPanel(mNotificationPanel);
+        ShowClearAll(false);
     }
 
     public void hideTilesPanel() {
@@ -223,6 +316,41 @@ public class PieStatusPanel {
 
     public void hideNotificationsPanel() {
         hidePanel(mNotificationPanel);
+    }
+
+    public void clearAll(){
+        if (mNotificationData != null) {
+            final boolean any = mNotificationData.size() > 0;
+            final boolean clearable = any && mNotificationData.hasClearableItems();
+            if (mCurrentViewState == QUICK_SETTINGS_PANEL) {
+                return; 
+            } else if (mClearButton.isShown()) {
+                if (clearable != (mClearButton.getAlpha() == 1.0f)) {
+                    ObjectAnimator clearAnimation = ObjectAnimator.ofFloat(
+                        mClearButton, "alpha", clearable ? 1.0f : 0.0f).setDuration(250);
+                    clearAnimation.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            if (mClearButton.getAlpha() <= 0.0f) {
+                                mClearButton.setVisibility(View.INVISIBLE);
+                            }
+                        }
+
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            if (mClearButton.getAlpha() <= 0.0f) {
+                                mClearButton.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    });
+                    clearAnimation.start();
+                }
+            } else {
+                mClearButton.setAlpha(clearable ? 1.0f : 0.0f);
+                mClearButton.setVisibility(clearable ? View.VISIBLE : View.INVISIBLE);
+            }
+            mClearButton.setEnabled(clearable);
+        }
     }
 
     private void showPanel(View panel) {
@@ -279,6 +407,11 @@ public class PieStatusPanel {
     public void updatePanelConfiguration() {
         int padding = mContext.getResources().getDimensionPixelSize(R.dimen.pie_panel_padding);
         mScrollView.setPadding(padding,0,padding,0);
+    }
+
+    private void ShowClearAll(boolean show){
+        mClearButton.setAlpha(show ? 0.0f : 1.0f);
+        mClearButton.setVisibility(show ? View.INVISIBLE : View.VISIBLE);
     }
 
     public static WindowManager.LayoutParams getFlipPanelLayoutParams() {
